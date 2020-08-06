994ca66e2f88a54ad16574b7ddb7cb79e8f57a0b
github raamenzurururu <reku9696@gmail.com>
2020-08-06 20:20:12 +0900

    add first commit

---
+++ src/store/index.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from '@/api/index'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    blogs: []
+  },
+  mutations: {
+    FETCH_BLOGS(state, blogs) {
+      state.blogs = blogs
+    },
+    ADD_BLOG(state, blog) {
+      const blogs = state.blogs.concat(blog)
+      state.blogs = blogs
+    }
+  },
+  actions: {
+    async fetchBlogs({ commit }) {
+      await axios().get('/blogs')
+        .then(res => {
+          commit('FETCH_BLOGS', res.data) //アクセス後の処理
+        })
+        .catch(e => console.log(e))
+    },
+    async addBlog({ commit }, blog) {
+      const res = await axios().post('/blogs', blog)
+      const savedBlog = res.data
+      commit('ADD_BLOG', savedBlog)
+      return savedBlog
+    }
+  }
+})
+
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <h1>Welcome</h1>
+    <router-link to="/blogs">Go</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+  }
+}
+</script>
